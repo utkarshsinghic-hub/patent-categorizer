ce54032931ce2efaab1cc345b7ebb10103e97a0c
utkarshsinghic-hub <utkarshsingh.icuerious@gmail.com>
2026-09-01 13:31:07 +0530

    Update app.py

--- app.py
+++ app.py
@@ -52,7 +52,7 @@ Patent claim:
 
 
             response = client.models.generate_content(
-                model="gemini-3.6-flash",
+                model="gemini-2.5-flash",
                 contents=prompt
             )
 
